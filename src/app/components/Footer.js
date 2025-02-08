f9d41b3a415defb42c13d985f05a372b7e1ed431
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-02-07 19:10:36 -0500

    mod: footer bg color

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-lightBlue border-t border-white18 px-8 py-2 text-white text-center text-[12px]">
+    <div className="bg-darkBlue border-t border-white18 px-8 py-2 text-white text-center text-[12px]">
       <span className="font-bold mb-2">© 2024 Ismael Sarmiento</span>
       <br />
       <span>
